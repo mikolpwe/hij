69bab0d9dda0d68ac2252de92ff2edaebb9ff130
mikolpwe <mikolpwe@outlook.co.id>
2024-05-21 06:42:51 +0700

    Add files via upload

---
+++ install-c.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install browser chrome
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo -E bash -
+yum install nodejs
+
+
+# Install dependency
+npm i puppeteer
+npm install --save puppeteer
+sudo yum install pango.x86_64 libXcomposite.x86_64 libXcursor.x86_64 libXdamage.x86_64 libXext.x86_64 libXi.x86_64 libXtst.x86_64 cups-libs.x86_64 libXScrnSaver.x86_64 libXrandr.x86_64 GConf2.x86_64 alsa-lib.x86_64 atk.x86_64 gtk3.x86_64 xorg-x11-fonts-100dpi xorg-x11-fonts-75dpi xorg-x11-utils xorg-x11-fonts-cyrillic xorg-x11-fonts-Type1 xorg-x11-fonts-misc
+
+# done
+echo "run 'node index.js' to start."
